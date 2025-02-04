cc354329fbaaa44b5a94134e66d879b27660756b
Nithin Kumar Balisetty <nithinkumarbalisetty98@gmail.com>
2025-02-04 01:23:29 -0600

    Created components for Data View and Question View

---
+++ src/components/DataView.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DataView = () => {
+  return (
+    <div>
+        <p className="text-sm md:text-base lg:text-lg">Data View</p>
+    </div>
+  );
+};
+
+export default DataView;
---
+++ src/components/QuestionsView.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QuestionsView = () => {
+  return (
+    <div>
+        <p className="text-sm md:text-base lg:text-lg">Questions View</p>
+    </div>
+  );
+};
+
+export default QuestionsView;
